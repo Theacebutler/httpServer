4cd231b05cd7a9d593da3ba5f7e6a6c44fb0e64c
theacebutler <theacebutler@gmail.com>
2026-03-17 23:47:51 -0400

    add nice printing

--- cmd/main.go
+++ cmd/main.go
@@ -26,7 +26,10 @@ func main() {
 		}
 
 		fmt.Println("Request:")
-		fmt.Printf("Request line:\r\n%s", req.RequestLine)
+		fmt.Printf("Request line\r\n")
+		fmt.Printf("Method: %s\r\n", req.RequestLine.Method)
+		fmt.Printf("Target: %s\r\n", req.RequestLine.Target)
+		fmt.Printf("Version: %s\r\n", req.RequestLine.Version)
 		for key, val := range req.Headers.Pairs() {
 			fmt.Printf("%s: %s\r\n", key, val)
 		}
