02060bbd33ad933e84b0451c671877083e3ce0f4
theacebutler <theacebutler@gmail.com>
2026-03-18 20:11:07 -0400

    some basic HTML pre-made responses

---
+++ server/html.go
@@ -0,0 +1,50 @@
+package server
+
+func Respons200() []byte {
+	return []byte(`<html>
+  <head>
+    <title>200 OK</title>
+  </head>
+  <body>
+    <h1>Success!</h1>
+    <p>Your request was an absolute banger.</p>
+  </body>
+</html>`)
+
+}
+
+func Respons400() []byte {
+	return []byte(`<html>
+  <head>
+    <title>400 Bad Request</title>
+  </head>
+  <body>
+    <h1>Bad Request</h1>
+    <p>Your request honestly kinda sucked.</p>
+  </body>
+</html>`)
+}
+
+func Respons404() []byte {
+	return []byte(`<html>
+  <head>
+    <title>404 Not Found</title>
+  </head>
+  <body>
+    <h1>404 Not Found</h1>
+    <p>Page not found.</p>
+  </body>
+</html>`)
+}
+
+func Respons500() []byte {
+	return []byte(`<html>
+  <head>
+    <title>500 Internal Server Error</title>
+  </head>
+  <body>
+    <h1>Internal Server Error</h1>
+    <p>Okay, you know what? This one is on me.</p>
+  </body>
+</html>`)
+}
